7c7178ef74ab9e2cd1b226c9febe89dfec355c53
javier <javier_munoz_navarret@hotmail.com>
2016-09-21 19:43:48 -0300

    se crearon las carpetas y los archivos

---
+++ ejer1/main.js
@@ -0,0 +1,9 @@
+
+var    alumno1    =    {nombre:    "Matías",    edad:5}
+
+var    alumno2    =    alumno1
+
+alumno2.nombre    =    "Sebastián"
+
+console.log(alumno1)
+console.log(alumno2)
---
+++ ejer3/main.js
@@ -0,0 +1,12 @@
+
+var    alumno1    =    {nombre:    "Matías",    edad:5}
+
+console.log(alumno1)
+
+var    alumno2    =    alumno1
+console.log(alumno1)
+
+alumno2.nombre    =    "Sebastián"
+
+console.log(alumno1)
+console.log(alumno2)
